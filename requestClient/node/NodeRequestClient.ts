50b15bb1a065023656268b5db70224d4323e21cd
Jaakko Heusala <jhh@hg.fi>
2023-04-15 11:18:22 +0300

    NodeRequestClient: Minor code syntax & style fi

--- requestClient/node/NodeRequestClient.ts
+++ requestClient/node/NodeRequestClient.ts
@@ -199,7 +199,7 @@ export class NodeRequestClient implements RequestClientInterface {
     ) : Promise<ResponseEntity<JsonAny| undefined>> {
         switch (method) {
             case RequestMethod.GET:    return await this._jsonRequest(RequestMethod.GET    , url, headers, data).then(NodeRequestClient._successJsonEntityResponse);
-            case RequestMethod.POST:   return await this._jsonRequest(RequestMethod.POST    , url, headers, data).then(NodeRequestClient._successJsonEntityResponse);
+            case RequestMethod.POST:   return await this._jsonRequest(RequestMethod.POST   , url, headers, data).then(NodeRequestClient._successJsonEntityResponse);
             case RequestMethod.PATCH:  return await this._jsonRequest(RequestMethod.PATCH  , url, headers, data).then(NodeRequestClient._successJsonEntityResponse);
             case RequestMethod.PUT:    return await this._jsonRequest(RequestMethod.PUT    , url, headers, data).then(NodeRequestClient._successJsonEntityResponse);
             case RequestMethod.DELETE: return await this._jsonRequest(RequestMethod.DELETE , url, headers, data).then(NodeRequestClient._successJsonEntityResponse);
